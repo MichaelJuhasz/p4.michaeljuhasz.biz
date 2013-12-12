34ab4d0f04029fa29ad6fc7f00c680e7409ccae0
Michael Juhasz <michael.juhasz@gmail.com>
2013-12-11 20:20:43 -0500

    Reconfiged config

--- config/config.php
+++ config/config.php
@@ -31,7 +31,7 @@ A email designated to receive messages from the server. Examples:
 	define('SYSTEM_EMAIL', 'michael.juhasz@gmail.com'); 
 
 # Default DB name for this app
-	define('DB_NAME', "p4_michaeljuhasz_biz"); 
+	define('DB_NAME', "michae58_p4_michaeljuhasz_biz"); 
 
 # Timezone
 	define('TIMEZONE', 'America/New_York');
